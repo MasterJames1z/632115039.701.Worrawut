f161ca1f2408c5af2becce6abbc5dd37f94e4dbb
MasterJames1z <jameskater350@gmail.com>
2021-01-13 17:10:07 +0700

    Circle

---
+++ Circle.java
@@ -0,0 +1,36 @@
+public class Circle {
+
+    double x;
+    double y;
+    double radius;
+    double cc;
+    double ar;
+
+    public Circle(double x, double y) {
+    }
+
+    public void Radius(double rad) {
+        radius = rad;
+    }
+
+    public double Area() {
+        ar = Math.PI * Math.pow(radius, 2);
+        return ar;
+    }
+
+    public double Circum() {
+        cc = 2 * Math.PI * radius;
+        return cc;
+    }
+
+    public boolean Intersec(Circle cep) {
+        double d = Math.sqrt(Math.pow(x - cep.x, 2) + Math.pow(y - cep.y, 2));
+        if (d > radius - cep.radius) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
---
+++ MainCircle.java
@@ -0,0 +1,26 @@
+public class MainCircle {
+    public static void main(String[] args) {
+        Circle cir1 = new Circle(0.0, 0.0);
+        cir1.Radius(2.0);
+        System.out.println("Area of cir1 = " + cir1.Area());
+        System.out.println("Circumference of cir1 = " + cir1.Circum());
+
+        Circle cir2 = new Circle(1.0, 2.0);
+        cir2.Radius(3.0);
+        System.out.println("Area of cir2 = " + cir2.Area());
+        System.out.println("Circumference of cir2 = " + cir2.Circum());
+
+        Circle cir3 = new Circle(3.0, 5.0);
+        cir3.Radius(1.0);
+        System.out.println("Area of cir3 = " + cir3.Area());
+        System.out.println("Circumference of cir3 = " + cir3.Circum());
+
+        Circle cir4 = new Circle(-1.0, -10.0);
+        cir4.Radius(0.0);
+        System.out.println("Area of cir = " + cir4.Area());
+        System.out.println("Circumference of cir4 = " + cir4.Circum());
+
+        System.out.println("Is circle 1 intersec circle 2: " + cir1.Intersec(cir2));
+        System.out.println("Is circle 1 intersec circle 3: " + cir1.Intersec(cir3));
+    }
+}
