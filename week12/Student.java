bfeccf13ea145f9feff069e2f17337837017a039
MasterJames1z <jameskater350@gmail.com>
2021-03-08 17:07:20 +0700

    week12

---
+++ week12/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    String Name;
+    Integer Age;
+    Double GPA;
+
+    Student(){
+
+    }
+
+    Student(String name,Integer age,Double gpa){
+        this.Name = name;
+        this.Age = age;
+        this.GPA = gpa;   
+        
+        
+    }
+    
+}
